63da78dc33eb24f4514a161bd3b393aa4b1ccdbb
Vennis-F <hoanganhgo28062001@gmail.com>
2023-05-27 17:52:29 +0700

    learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const session = require("express-session");
-const MemcachedStore = require("connect-memjs")(session);
-const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const MongoDBStore = require("connect-mongodb-session")(session);
 const dotenv = require("dotenv");
 dotenv.config();
 const frameguard = require("frameguard");
@@ -22,12 +19,6 @@ const feedbackRouter = require("./routers/feedback");
 const sliderRouter = require("./routers/slider");
 const customerRouter = require("./routers/customer");
 const dashboardRouter = require("./routers/dashboard");
-const memjs = require("memjs");
-const mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
-  failover: true, // default: false
-  timeout: 1, // default: 0.5 (seconds)
-  keepAlive: true, // default: false
-});
 const app = express();
 const port = process.env.PORT || 6969;
 
@@ -36,58 +27,24 @@ app.use(frameguard({ action: "SAMEORIGIN" }));
 require("./db/mongoose");
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:5000" }));
-app.use(cookieParser("ClydeIsASquirrel"));
 
 //Session
-// const uri = process.env.MONGGO_DOMAIN;
-// const store = new MongoDBStore({
-//   uri,
-//   collection: "sessions",
-//   databaseName: "Bookstore",
-// });
-// store.on("error", function (error) {
-//   console.log("[Session Mongodb store is running]", error);
-// });
-app.set("trust proxy", 1);
 app.use(
   session({
-    secret: "ClydeIsASquirrel",
-    resave: "false",
-    saveUninitialized: "false",
+    secret: "Asu",
+    resave: true,
+    saveUninitialized: false,
     cookie: {
       maxAge: 18000000000,
-      secure:
-        process.env.NODE_ENV && process.env.NODE_ENV == "production"
-          ? true
-          : false,
+      secure: false,
     },
-    store: new MemcachedStore({
-      servers: [process.env.MEMCACHIER_SERVERS],
-      prefix: "_session_",
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGGO_DOMAIN, //YOUR MONGODB URL
+      // ttl: 14 * 24 * 60 * 60,
+      autoRemove: "native",
     }),
   })
 );
-// app.use(
-//   session({
-//     secret: "Asu",
-//     resave: true,
-//     saveUninitialized: false,
-//     cookie: {
-//       maxAge: 18000000000,
-//       secure:
-//         process.env.NODE_ENV && process.env.NODE_ENV == "production"
-//           ? true
-//           : false,
-//     },
-// store: MongoStore.create({
-//   mongoUrl: uri, //YOUR MONGODB URL
-//   // ttl: 14 * 24 * 60 * 60,
-//   autoRemove: "native",
-//   dbName: "Bookstore",
-// }),
-// store,
-//   })
-// );
 
 //Config express
 app.use("/user", userRouter);
@@ -104,56 +61,6 @@ app.use("/feedbacks", feedbackRouter);
 app.use("/sliders", sliderRouter);
 app.use("/customers", customerRouter);
 app.use("/dashboards", dashboardRouter);
-
-app.get("/demo-session", function (req, res, next) {
-  if (req.session.views) {
-    req.session.views++;
-    res.setHeader("Content-Type", "text/html");
-    res.write("<p>views: " + req.session.views + "</p>");
-    res.write("<p>expires in: " + req.session.cookie.maxAge / 1000 + "s</p>");
-    res.end();
-  } else {
-    req.session.views = 1;
-    res.end("welcome to the session demo. refresh!");
-  }
-});
-
-//set session
-app.get("/set_session", (req, res) => {
-  //set a object to session
-  req.session.User = {
-    website: "anonystick.com",
-    type: "blog javascript",
-    like: "4550",
-  };
-
-  return res.status(200).json({ status: "success", ssId: req.session.id });
-});
-
-//set session
-app.get("/get_session", (req, res) => {
-  //check session
-  console.log(123);
-  req.session;
-  if (req.session) {
-    return res
-      .status(200)
-      .json({ status: "success", session: req.session, ssId: req.session.id });
-  }
-  return res.status(200).json({ status: "error", session: "No session" });
-});
-
-//Delete session
-app.get("/destroy_session", (req, res) => {
-  // req.session.views = ""
-  // return res.status(200).json({ status: "success" })
-  //destroy session
-  req.session.destroy(function (err) {
-    return res
-      .status(200)
-      .json({ status: "success", session: "cannot access session here" });
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

8c754870ac76cf1497b186ef2c86cba23af46181
Vennis-F <hoanganhgo28062001@gmail.com>
2023-05-27 13:26:14 +0700

    hihi

--- src/utils/session.js
+++ src/utils/session.js
@@ -1,15 +1,15 @@
 app.get("/demo-session", function (req, res, next) {
   if (req.session.views) {
-    req.session.views++
-    res.setHeader("Content-Type", "text/html")
-    res.write("<p>views: " + req.session.views + "</p>")
-    res.write("<p>expires in: " + req.session.cookie.maxAge / 1000 + "s</p>")
-    res.end()
+    req.session.views++;
+    res.setHeader("Content-Type", "text/html");
+    res.write("<p>views: " + req.session.views + "</p>");
+    res.write("<p>expires in: " + req.session.cookie.maxAge / 1000 + "s</p>");
+    res.end();
   } else {
-    req.session.views = 1
-    res.end("welcome to the session demo. refresh!")
+    req.session.views = 1;
+    res.end("welcome to the session demo. refresh!");
   }
-})
+});
 
 //set session
 app.get("/set_session", (req, res) => {
@@ -18,21 +18,22 @@ app.get("/set_session", (req, res) => {
     website: "anonystick.com",
     type: "blog javascript",
     like: "4550",
-  }
+  };
 
-  return res.status(200).json({ status: "success" })
-})
+  return res.status(200).json({ status: "success" });
+});
 
 //set session
 app.get("/get_session", (req, res) => {
   //check session
+  console.log(123);
   if (req.session.User) {
     return res
       .status(200)
-      .json({ status: "success", session: req.session.User })
+      .json({ status: "success", session: req.session.User });
   }
-  return res.status(200).json({ status: "error", session: "No session" })
-})
+  return res.status(200).json({ status: "error", session: "No session" });
+});
 
 //Delete session
 app.get("/destroy_session", (req, res) => {
@@ -42,6 +43,6 @@ app.get("/destroy_session", (req, res) => {
   req.session.destroy(function (err) {
     return res
       .status(200)
-      .json({ status: "success", session: "cannot access session here" })
-  })
-})
+      .json({ status: "success", session: "cannot access session here" });
+  });
+});
